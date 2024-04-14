bc32f43f22d7e1715ef0cc1453c4f1a38fb170d2
Dhilla <jackson337079@gmail.com>
2024-04-14 10:59:57 +0530

    base64dec

---
+++ Pass Decryption.py
@@ -0,0 +1,9 @@
+import base64
+
+def decrypt(passw):
+    dec_data = base64.b64decode(passw)
+    dec_pass = dec_data.decode()
+    print(dec_pass)
+
+password = input("enter the encrypted password: ")
+decrypt(password)
